c89cb5d546eb673dba113e630f249efe7bda0230
stanordan <stanordan@gmail.com>
2017-03-30 08:49:24 -0400

    another update
    Added some more instructions for using this on org mode files.

--- makeNice.js
+++ makeNice.js
@@ -5,6 +5,13 @@ function makeNiceMyHtmlEntities() {
 }
 window.onload = makeNiceMyHtmlEntities;
 /* 
+INSTRUCTIONS FOR ADDDING THIS TO ORG MODE FILES
+===============================================
+
+Can be added remotely:
+
 #+OPTIONS: -:nil
 #+HTML_HEAD: <script type="text/javascript" src="https://raw.githubusercontent.com/stanordan/scripts/master/makeNice.js"></script>
+
+Or surround it with script tags to load it locally.
 */
