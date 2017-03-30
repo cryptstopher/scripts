f4a53fee7ccf13d176979a0be363b3a830011e4b
stanordan <stanordan@gmail.com>
2017-03-30 08:33:26 -0400

    update makeNice
    Just deleted some lines

--- makeNice.js
+++ makeNice.js
@@ -3,9 +3,7 @@ function makeNiceMyHtmlEntities() {
     var makingNice = fun.replace(/ -- /g, "&mdash;");
     document.getElementById("content").innerHTML = makingNice;
 }
-
 window.onload = makeNiceMyHtmlEntities;
-
 /* 
 #+OPTIONS: -:nil
 #+HTML_HEAD: <script type="text/javascript" src="https://raw.githubusercontent.com/stanordan/scripts/master/makeNice.js"></script>
